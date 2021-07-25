a16350ee660abb1289bd5fbfcacb34ebf6ebbe24
HC Geum <49156892+Doyosae@users.noreply.github.com>
2021-07-26 04:16:49 +0900

    Update pose_decoder.py

--- model_layer/pose_decoder.py
+++ model_layer/pose_decoder.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional  as F
+import torch.nn.functional as F
 from collections import OrderedDict
 
 
